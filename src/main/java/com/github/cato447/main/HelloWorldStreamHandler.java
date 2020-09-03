f04f9ff812e0ac6a4c95dbb1da8fbcb8601faa50
Simon <simon@bussmann.es>
2020-09-03 21:38:08 +0200

    Initialized the project for my Alexa Skill

---
+++ src/main/java/com/github/cato447/main/HelloWorldStreamHandler.java
@@ -0,0 +1,27 @@
+package com.github.cato447.main;
+
+import com.amazon.ask.Skill;
+import com.amazon.ask.SkillStreamHandler;
+import com.amazon.ask.Skills;
+import com.github.cato447.handlers.*;
+
+
+public class HelloWorldStreamHandler extends SkillStreamHandler {
+
+    private static Skill getSkill(){
+       return Skills.standard()
+               .addRequestHandlers(
+                       new CancelandStopIntentHandler(),
+                       new HelloWorldIntentHandler(),
+                       new HelpIntentHandler(),
+                       new LaunchRequestHandler(),
+                       new SessionEndedRequestHandler())
+               .withSkillId("Your Amazon ID")
+               .build();
+   }
+
+    public HelloWorldStreamHandler(Skill skill) {
+        super(skill);
+    }
+
+}
